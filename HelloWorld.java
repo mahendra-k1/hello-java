45f058ed9c88f682ff7ffd5958df1f01a4798fa4
mahendra-k1 <mahendra4int@gmail.com>
2025-05-10 14:54:48 +0530

    Create HelloWorld.java
    my first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + GitHub!");
+    }
+
+    public static String greet() {
+        return "Hello from method!";
+    }
+}
